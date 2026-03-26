3c6ada2a11adf35bba151601b8220f61890b1ea5
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-26 11:03:06 +0900

    既存商品の商品名を修正。

--- src/main/java/com/app/model/CampaignFeed.java
+++ src/main/java/com/app/model/CampaignFeed.java
@@ -6,6 +6,7 @@ public class CampaignFeed {
 
   public static List<Campaign> list() {
     return List.of(
+        new Campaign("2026-03-25", "春の感謝セール：全品15%OFF（3/31まで）"),
         new Campaign("2025-12-28", "年末セール：全品10%OFF（12/31まで）"),
         new Campaign("2025-12-25", "送料無料キャンペーン（3,000円以上）"),
         new Campaign("2025-12-01", "新規会員クーポン：500円OFF")
