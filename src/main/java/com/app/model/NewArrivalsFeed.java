6b0b37218b8bd2412a18dd569d536e705b1392b6
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-26 13:35:07 +0900

    コード再修正（コンフリクト再現のため）

--- src/main/java/com/app/model/NewArrivalsFeed.java
+++ src/main/java/com/app/model/NewArrivalsFeed.java
@@ -9,7 +9,7 @@ public class NewArrivalsFeed {
 
   public static List<NewArrival> list() {
     return List.of(
-        new NewArrival("2025-12-28", "ワイヤレスイヤホン Pro X123"),
+        new NewArrival("2025-12-28", "ワイヤレスイヤホン Pro X111"),
         new NewArrival("2025-12-20", "軽量ノートPC 14インチ"),
         new NewArrival("2025-12-10", "USB-C 充電器 65W"),
         new NewArrival("2026-03-25", "iPad Air m3モデル 512GB")
