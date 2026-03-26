45513a964c10b95ace6c837a35bba58285f75988
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-26 12:09:23 +0900

    編集間違えのため機微な修正実施

--- src/main/java/com/app/model/NewArrivalsFeed.java
+++ src/main/java/com/app/model/NewArrivalsFeed.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 public class NewArrivalsFeed {
 
+  private NewArrivalsFeed() {
+  }
+
   public static List<NewArrival> list() {
     return List.of(
         new NewArrival("2025-12-28", "ワイヤレスイヤホン Pro X"),
         new NewArrival("2025-12-20", "軽量ノートPC 14インチ"),
         new NewArrival("2025-12-10", "USB-C 充電器 65W"),
-        new NewArrival("2026-03-25", "iPad Air m4モデル 512GB")
+        new NewArrival("2026-03-25", "iPad Air m3モデル 512GB")
     );
   }
-
-  private NewArrivalsFeed() {}
 }
